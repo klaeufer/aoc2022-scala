b10d85c1ccede77ff96ad55495e428efc88f937a
Konstantin Läufer <laufer@cs.luc.edu>
2023-01-24 21:42:45 -0600

    simplified main match/case and added some comments
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/main/scala/Day7.scala
+++ src/main/scala/Day7.scala
@@ -1,16 +1,16 @@
-import scala.util.matching.Regex
-
 object Day7 extends App {
 
   // part 1
 
   val input = scala.io.Source.fromFile("input.txt").getLines.toList
 
+  // regex patterns for matching input lines
   val cd = "\\$ cd (.+)".r
   val ls = "\\$ ls".r
   val dir = "dir (.+)".r
   val file = "(\\d+) (.+)".r
 
+  // as a warmup, determine total sum of file sizes
   val inUse = input.foldLeft(0) { case (r, l) =>
     l match
       case file(k, _) => r + k.toInt
@@ -19,20 +19,21 @@ object Day7 extends App {
 
   println(f"disk space in use = $inUse")
 
+  // q: current dir stack
+  // r: processed dirs with their total subtree sizes
   val (q, r) = input.foldLeft(Seq.empty[(String, Int)], Seq.empty[(String, Int)]) { case ((q, r), l) =>
     l match
-      case cd("/") => (("/" -> 0) +: q, r)
       case cd("..") => ((q.tail.head._1 -> (q.tail.head._2 + q.head._2)) +: q.tail.tail, q.head +: r)
       case cd(d1) => ((d1 -> 0) +: q, r)
-      case ls() => (q, r)
-      case dir(_) => (q, r)
       case file(k, _) => ((q.head._1 -> (q.head._2 + k.toInt)) +: q.tail, r)
+      case _ => (q, r)
   }
 
   println(q)
   println(r)
 
-  val r1 = q.foldLeft(q :+ ("|" -> -1), r) { case ((q, r), _) =>
+  // process directories left in dir stack (equivalent of successive cd .. back to /)
+  val r1 = q.foldLeft(q :+ (".DUMMY" -> -1), r) { case ((q, r), _) =>
     ((q.tail.head._1 -> (q.tail.head._2 + q.head._2)) +: q.tail.tail, q.head +: r)
   }
 
