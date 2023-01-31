06a02fc7fd1d048087c43626a1d1ae9696a43113
Konstantin Läufer <laufer@cs.luc.edu>
2023-01-30 19:23:42 -0600

    minor cosmetic improvements
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/test/scala/Day9Test.scala
+++ src/test/scala/Day9Test.scala
@@ -36,15 +36,15 @@ class Day9Test extends AnyPropSpec, TableDrivenPropertyChecks, should.Matchers:
   )
 
   def example1 =
-      """R 4
-        |U 4
-        |L 3
-        |D 1
-        |R 4
-        |D 1
-        |L 5
-        |R 2
-        |""".stripMargin('|').split('\n').toIndexedSeq
+    """R 4
+      |U 4
+      |L 3
+      |D 1
+      |R 4
+      |D 1
+      |L 5
+      |R 2
+      |""".stripMargin('|').split('\n').toIndexedSeq
 
   def example2 =
     """R 5
