e64f9d7823c4a579c1c4c038817cc005653df287
Konstantin Läufer <laufer@cs.luc.edu>
2023-02-21 19:54:36 -0600

    simplified by introducing indexWhere
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/main/scala/Day6.scala
+++ src/main/scala/Day6.scala
@@ -4,9 +4,9 @@ object Day6 extends App:
 
   val input = scala.io.Source.fromFile("AdventOfCodeDay6Input.txt").mkString
 
-  def f(input: String) = input.sliding(4).zipWithIndex.takeWhile((s, i) => s.toSet.size < 4).toSeq.last._2 + 5
+  def f(input: String) = input.sliding(4).indexWhere(_.toSet.size == 4) + 4
 
-  def g(input: String) = input.sliding(14).zipWithIndex.takeWhile((s, i) => s.toSet.size < 14).toSeq.last._2 + 15
+  def g(input: String) = input.sliding(14).indexWhere(_.toSet.size == 14) + 14
 
   println(f(input))
 
