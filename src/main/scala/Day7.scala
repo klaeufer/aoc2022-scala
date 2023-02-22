38ae799cbf9b287c98ff814543260e9d17739ca4
Konstantin Läufer <laufer@cs.luc.edu>
2023-02-21 19:56:30 -0600

    flattened pattern matching for day 7
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/main/scala/Day7.scala
+++ src/main/scala/Day7.scala
@@ -11,22 +11,20 @@ object Day7 extends App:
   val file = "(\\d+) (.+)".r
 
   // as a warmup, determine total sum of file sizes
-  val inUse = input.foldLeft(0) { case (r, l) =>
-    l match
-      case file(k, _) => r + k.toInt
-      case _ => r
+  val inUse = input.foldLeft(0) {
+    case (r, file(k, _)) => r + k.toInt
+    case (r, _) => r
   }
 
   println(f"disk space in use = $inUse")
 
   // q: current dir stack
   // r: processed dirs with their total subtree sizes
-  val (q, r) = input.foldLeft(Seq.empty[(String, Int)], Seq.empty[(String, Int)]) { case ((q, r), l) =>
-    l match
-      case cd("..") => ((q.tail.head._1 -> (q.tail.head._2 + q.head._2)) +: q.tail.tail, q.head +: r)
-      case cd(d1) => ((d1 -> 0) +: q, r)
-      case file(k, _) => ((q.head._1 -> (q.head._2 + k.toInt)) +: q.tail, r)
-      case _ => (q, r)
+  val (q, r) = input.foldLeft(Seq.empty[(String, Int)], Seq.empty[(String, Int)]) {
+    case ((q, r), cd("..")) => ((q.tail.head._1 -> (q.tail.head._2 + q.head._2)) +: q.tail.tail, q.head +: r)
+    case ((q, r), cd(d1)) => ((d1 -> 0) +: q, r)
+    case ((q, r), file(k, _)) => ((q.head._1 -> (q.head._2 + k.toInt)) +: q.tail, r)
+    case (s, _) => s
   }
 
   println(q)
@@ -39,12 +37,12 @@ object Day7 extends App:
 
   println(r1._2)
 
-  println(r1._2.map(_._2).filter(_ <= 100000).sum)
+  println(r1._2.map(_._2).filter(_ <= 100_000).sum)
 
   // part 2
 
-  val total = 70000000
-  val need = 30000000
+  val total = 70_000_000
+  val need = 30_000_000
   val free = total - inUse
   println(f"free space = $free")
   val toDelete = need - free
