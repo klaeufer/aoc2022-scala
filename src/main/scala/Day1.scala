5057ad517b434f65fa0a3d1084679ce3c02e77c2
Konstantin Läufer <laufer@cs.luc.edu>
2023-02-22 23:14:31 -0600

    renamed day 1 extension method
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -2,7 +2,7 @@ object Day1 extends App:
 
   /** Partitions an iterator into chunks of consecutive elements for which the predicate holds. */
   extension [A](it: Iterator[A])
-    def chunkBy(p: A => Boolean) = Iterator
+    def splitWhere(p: A => Boolean) = Iterator
       .continually(it.takeWhile(p))
       .takeWhile(_ => it.hasNext)
 
@@ -11,7 +11,7 @@ object Day1 extends App:
   // iterate over inventories of consecutive nonempty lines
   // this stores only one Int per elf in memory
   val result = input
-    .chunkBy(_.nonEmpty)
+    .splitWhere(_.nonEmpty)
     .map(_.map(_.toInt).sum)
     .toIndexedSeq
     .sorted
@@ -19,4 +19,4 @@ object Day1 extends App:
   println(s"Day 1 part 1: ${result.last}")
   println(s"Day 1 part 2: ${result.takeRight(3).sum}")
 
-  println(Iterator(5, 2, 4, 3, 8, 10, 7, 7, 7, 12, 14, 20, 3).chunkBy(_ % 2 == 0).map(_.length).toSeq)
+  println(Iterator(5, 2, 4, 3, 8, 10, 7, 7, 7, 12, 14, 20, 3).splitWhere(_ % 2 == 0).map(_.length).toSeq)
